9b417dc1f91d1f31bfaf632b0d65cd74e2f4aff0
zhangxiaoxu <31872163@qq.com>
2024-05-17 14:57:52 +0800

    running.sh

--- runing.sh
+++ runing.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# while : 
-# do
-for i in {1..100}
-do
-echo "$RANDOM"
-done
-# sleep 1
-# done
+ while : 
+ do
+    for i in {1..100}
+    do
+    echo "$RANDOM"
+    done
+ sleep 1
+ done
